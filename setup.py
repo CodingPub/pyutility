7662717cb55eba0fe07c6a36011cdeecf911aee9
linxiaobin <linxiaobin@felink.com>
2016-11-14 14:36:24 +0800

    logger

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='utility',
+      version='1.0',
+      description='',
+      author='LinXiaobin',
+      author_email='linxiaobin0605@gmail.com',
+      url='http://blog.liuts.com',
+      py_modules=['__init__', 'logger'])
